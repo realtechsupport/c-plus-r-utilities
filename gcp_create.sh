1e3019ebd8843a5c25bc739588ec9a269e5ebaf7
RTS <25514511+realtechsupport@users.noreply.github.com>
2020-05-16 08:51:21 -0400

    Delete gcp_create.sh

--- gcp_create.sh
+++
@@ -1,9 +0,0 @@
-MY_INSTANCE_NAME="catch-release2"
-ZONE=us-west1-b
-
-gcloud compute instances create $MY_INSTANCE_NAME \
-    --image-family=ubuntu-1804-lts \
-    --image-project=ubuntu-os-cloud \
-    --machine-type=e2-medium \
-    --zone $ZONE \
-    --tags http-server
